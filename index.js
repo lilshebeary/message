8e6f6f252d1ac077c80fbccf973ac5ea56101708
Ciri Ryan <cirijaye@yahoo.com>
2022-02-06 14:54:47 -0800

    finish app add now so that others can use app

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+
+  document.querySelector('h1').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', event => {
-    event.preventDefault();
+  event.preventDefault();
 
-    document.querySelector('#message-form').classList.add('hide');
-    document.querySelector('#link-form').classList.remove('hide')
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
 
-    const input = document.querySelector('#message-input');
-    const encrypted = btoa(input.value);
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
 
-    linkInput = document.querySelector('#link-input')
-    
-    linkInput.value = `${window.location}#${encrypted}`;
-    linkInput.select();
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
+
+// https://message-lilshebeary.vercel.app
