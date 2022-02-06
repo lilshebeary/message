2abd14f8786ffd2d0c9b6b2cf090e30d3f82f7d2
Ciri Ryan <cirijaye@yahoo.com>
2022-02-06 09:33:50 -0800

    add and select encrypted link, style

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide')
+
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
-    document.querySelector('#link-input').value = encrypted;
+    linkInput = document.querySelector('#link-input')
+    
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
 });
